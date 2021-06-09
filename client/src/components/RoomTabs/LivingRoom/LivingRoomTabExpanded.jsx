28558d9b436ac660a1270590594696d6acd6132e
zygimantas88 <zygimantas.karecka@gmail.com>
2021-06-09 14:52:19 +0300

    feat: implemented living room lights buttons

--- client/src/components/RoomTabs/LivingRoom/LivingRoomTabExpanded.jsx
+++ client/src/components/RoomTabs/LivingRoom/LivingRoomTabExpanded.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles.LivingRoom.css';
 // Icons
 import icon from '../../../img/icons/sofa.svg';
 import cancel from '../../../img/icons/cancel.svg';
 // Light button icons
-import mainLampOff from '../../../img/icons/LightsIcons/hang-lamp-off.svg';
-import mainLampOn from '../../../img/icons/LightsIcons/hang-lamp-on.svg';
+import lampOff from '../../../img/icons/LightsIcons/lamp-off.svg';
+import lampOn from '../../../img/icons/LightsIcons/lamp-on.svg';
 // Components
 import Temperature from '../../Temperature/Temperature';
 import LightBtn from '../../Buttons/Lights/LightsBtn'
@@ -14,13 +14,29 @@ import LightBtn from '../../Buttons/Lights/LightsBtn'
 const LivingRoomTabExpanded = ({ setLivingRoomTab }) => {
 
     const [mainLight, setMainLight] = useState(false);
+    const [diningTableLights, setdiningTableLights] = useState(false);
+    const [secondaryLights, setSecondaryLights] = useState(false);
+    const [curtainsLights, setCurtainsLights] = useState(false);
+    const [allLights, setAllLights] = useState(false);
 
-    const handleBtnClick = () => {
-        mainLight ? setMainLight(false) : setMainLight(true)
-    };
+    const allLightsOn = () => {
+        setAllLights(true);
+        setMainLight(true);
+        setdiningTableLights(true);
+        setSecondaryLights(true);
+        setCurtainsLights(true);
+        // Here will be code which will send a command to turn on all lights
+    }
 
-    // sets lamp icon to On or Off state
-    const iconState = mainLight ? mainLampOn : mainLampOff;
+    const allLightsOff = () => {
+        setAllLights(false);
+        setMainLight(false);
+        setdiningTableLights(false);
+        setSecondaryLights(false);
+        setCurtainsLights(false);
+        // Here will be code which will send a command to turn off all lights
+
+    }
 
     const handleCloseTab = (e) => {
         e.stopPropagation();
@@ -37,12 +53,31 @@ const LivingRoomTabExpanded = ({ setLivingRoomTab }) => {
             </div>
             <Temperature />
             <div className="grid-container">
-                <LightBtn handleBtnClick={handleBtnClick} iconState={iconState} btnName={'Main light'}/>
-                <LightBtn />
-                <LightBtn />
-                <LightBtn />
-                <LightBtn />
-                <LightBtn />
+                <LightBtn
+                    handleBtnClick={() => mainLight ? setMainLight(false) : setMainLight(true)}
+                    iconState={mainLight ? lampOn : lampOff} // sets lamp icon to On or Off state
+                    btnName={"Main light"}
+                />
+                <LightBtn
+                    handleBtnClick={() => diningTableLights ? setdiningTableLights(false) : setdiningTableLights(true)}
+                    iconState={diningTableLights ? lampOn : lampOff}
+                    btnName={"Dining table"}
+                />
+                <LightBtn
+                    handleBtnClick={() => secondaryLights ? setSecondaryLights(false) : setSecondaryLights(true)}
+                    iconState={secondaryLights ? lampOn : lampOff}
+                    btnName={"Secondary lights"}
+                />
+                <LightBtn
+                    handleBtnClick={() => curtainsLights ? setCurtainsLights(false) : setCurtainsLights(true)}
+                    iconState={curtainsLights ? lampOn : lampOff}
+                    btnName={"Curtains LED"}
+                />
+                <LightBtn
+                    handleBtnClick={allLights ? allLightsOff : allLightsOn}
+                    iconState={allLights ? lampOn : lampOff}
+                    btnName={"All lights"}
+                />
             </div>
         </div>
     )
